8ae4768c1f4416094586fe80839168d7f50badc4
goutamdash <dashgoutam2003@gmail.com>
2025-07-28 05:15:35 +0530

    week6 solutions

---
+++ Code/LAB_3/App.js
@@ -0,0 +1,14 @@
+// Week_6/Code/LAB_3/App.js
+
+import React from 'react';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore />
+    </div>
+  );
+}
+
+export default App;
